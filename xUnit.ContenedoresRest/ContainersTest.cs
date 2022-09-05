aaadbac3ae5ebeaa6fab6608535076c6fdc63bf4
lcosoriore <luisa.osoriore@gmail.com>
2022-09-05 13:15:30 -0500

    4

--- xUnit.ContenedoresRest/ContainersTest.cs
+++ xUnit.ContenedoresRest/ContainersTest.cs
@@ -8,50 +8,50 @@ namespace xUnit.ContenedoresRest
         //    /// <summary>
         //    /// Happy path
         //    /// </summary>
-        //    [Fact]
-        //    public void Containers_selectContainers_ContainersDespachados()
-        //    {
-        //        double budget = 1508.65;
-        //        List<ContainersModel> objModel = new List<ContainersModel>();
-        //        objModel.Add(new ContainersModel
-        //        {
-        //            name="c1",
-        //            containerPrice= 4744.03,
-        //            transportCost= 571.4
+        [Fact]
+        public void Containers_selectContainers_ContainersDespachados()
+        {
+            double budget = 1508.65;
+            List<ContainersModel> objModel = new List<ContainersModel>();
+            objModel.Add(new ContainersModel
+            {
+                name = "c1",
+                containerPrice = 4744.03,
+                transportCost = 571.4
 
-        //        });
-        //        objModel.Add(new ContainersModel
-        //        {
-        //            name = "c2",
-        //            containerPrice = 3579.07,
-        //            transportCost = 537.33
+            });
+            objModel.Add(new ContainersModel
+            {
+                name = "c2",
+                containerPrice = 3579.07,
+                transportCost = 537.33
 
-        //        });
-        //        objModel.Add(new ContainersModel
-        //        {
-        //            name = "c3",
-        //            containerPrice = 3579.07,
-        //            transportCost = 537.33
+            });
+            objModel.Add(new ContainersModel
+            {
+                name = "c3",
+                containerPrice = 3579.07,
+                transportCost = 537.33
 
-        //        });
-        //        objModel.Add(new ContainersModel
-        //        {
-        //            name = "c4",
-        //            containerPrice = 1700.12,
-        //            transportCost = 247.28
+            });
+            objModel.Add(new ContainersModel
+            {
+                name = "c4",
+                containerPrice = 1700.12,
+                transportCost = 247.28
 
-        //        });
-        //        objModel.Add(new ContainersModel
-        //        {
-        //            name = "c5",
-        //            containerPrice = 1434.80,
-        //            transportCost = 264.54
+            });
+            objModel.Add(new ContainersModel
+            {
+                name = "c5",
+                containerPrice = 1434.80,
+                transportCost = 264.54
 
-        //        });
-        //        StatsManagerl objContainer = new StatsManagerl();
-        //        var lstContainers= objContainer.selectContainers(budget, objModel);
-        //        //Assert.Equal(new string[] { "c1", "c2", "c4" }, lstContainers);
-        //    }
+            });
+            StatsManagerl objContainer = new StatsManagerl();
+           // var lstContainers = objContainer.selectContainers(budget, objModel);
+            //Assert.Equal(new string[] { "c1", "c2", "c4" }, lstContainers);
+        }
         //    /// <summary>
         //    /// Budget o
         //    /// </summary>
