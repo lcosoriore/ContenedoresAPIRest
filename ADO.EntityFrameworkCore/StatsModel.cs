1775b6e0d81719445c7412c20b500f5035d587da
lcosoriore <luisa.osoriore@gmail.com>
2022-09-06 16:39:12 -0500

    5

--- ADO.EntityFrameworkCore/StatsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADO.EntityFrameworkCore.Models
-{
-    public class StatsModel
-    {
-        public double containers_dispatched { get; set; }
-        public double containers_not_dispatched { get; set; }
-        public double budget_used   { get; set; }
-    }
-}
